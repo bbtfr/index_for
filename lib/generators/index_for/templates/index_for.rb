c899d58312ce9eba18351628a661a8b53571a438
Theo <bbtfrr@gmail.com>
2014-08-26 09:45:08 +0800

    update index_for configuration file for rails generator

--- lib/generators/index_for/templates/index_for.rb
+++ lib/generators/index_for/templates/index_for.rb
@@ -1,24 +1,59 @@
 # Use this setup block to configure all options available in IndexFor.
 IndexFor.setup do |config|
-  # The tag which wraps index_for calls.
-  # config.index_for_tag = :div
 
-  # The DOM class set for index_for tag. Default is nil
-  # config.index_for_class = :custom
+  # IndexFor
+
+  # # The tag which wraps index_for calls.
+  # config.table_tag = :table
+  # config.table_class = nil
   
-  # The tag which wraps header call. Default is :thead, set nil to turn off header.
-  # config.head_tag = nil
+  # config.table_head_tag = :thead
+  # config.table_head_class = nil
 
-  # The tag which wraps body call. Default is :tbody.
-  # config.body_tag = :ul
+  # config.table_body_tag = :tbody
+  # config.table_body_class = nil
 
-  # The tag which wraps each head column call. Default is :th.
-  # config.head_column_tag = :li
+  # config.table_row_tag = :tr
+  # config.table_row_class = nil
 
-  # The tag which wraps each body column call. Default is :td.
-  # config.body_column_tag = :li
-
-  # The tag which wraps each row call. Default is :tr.
-  # config.row_tag = :p
+  # config.table_head_cell_tag = :th
+  # config.table_head_cell_class = nil
   
+  # config.table_body_cell_tag = :td
+  # config.table_body_cell_class = nil
+
+  # # The classname for actions cells.
+  # config.table_actions_cell_class = :actions
+
+  # config.action_link_class = :action
+
+  # ShowFor
+
+  # # The tag which wraps show_for calls.
+  # config.list_tag = :dl
+  # config.list_class = nil
+
+  # config.list_row_tag = nil
+  # config.list_row_class = nil
+
+  # config.list_label_tag = :dt
+  # config.list_label_class = nil
+
+  # config.list_content_tag = :dd
+  # config.list_content_class = nil
+
+  # config.list_label_proc = nil
+
+  # config.blank_content_class = "blank"
+
+  # # The default time format for I18n.
+  # config.i18n_format = :default
+
+  # # The methods which used for displaying the associations.
+  # config.association_methods = [ :name, :title, :to_s ]
+
+  # # The tag which wraps the collections.
+  # config.collection_tag = :ul
+  # config.collection_column_tag = :li
+
 end
